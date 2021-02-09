8c694841ef30e2ccef891c4220407114c670c1df
Martin Wam <martin.wam@gmail.com>
2021-02-09 14:49:20 +0100

    Count consumer

---
+++ src/main/java/no/digdir/efmstatisticsclient/CountConsumer.java
@@ -0,0 +1,38 @@
+package no.digdir.efmstatisticsclient;
+
+import lombok.RequiredArgsConstructor;
+import no.digdir.efmstatisticsclient.domain.StatusKey;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Properties;
+
+@Component
+@RequiredArgsConstructor
+public class CountConsumer {
+    private final Properties consumerProperties;
+    private final Serde<StatusKey> statusKeySerde;
+
+    @PostConstruct
+    public void statusCount() {
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<StatusKey, Long> countStream = builder.stream("status-count", Consumed.with(statusKeySerde, Serdes.Long()));
+        countStream.foreach((k,v) -> {
+            // TODO - process stream, push update to tidsserie
+            System.out.println("Update - key="+k+", value="+v);
+        });
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), consumerProperties);
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+}
